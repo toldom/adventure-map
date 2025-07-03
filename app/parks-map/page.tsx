6fe9d97c981bc4253424e26ea503de2100f0cd8b
Marcus Toldo <33906542+toldom@users.noreply.github.com>
2025-07-03 02:28:19 -0400

    Added SSI Maps
    - Added SSI Map images
    - Loading SSI Maps on map pages

--- app/parks-map/page.tsx
+++ app/parks-map/page.tsx
@@ -2,16 +2,19 @@ import styles from "@/app/page.module.scss";
 import Image from "next/image";
 import Header from "@/components/Header";
 import LinkCard from "@/components/LinkCard";
+import { config } from "@/data/config";
+
+const envMetadata = config.envMetadata;
 
 export default function ParksMap() {
 	return (
 		<div>
-			<Header mainLine="Parks" subLine="Map" />
+			<Header mainLine={envMetadata.mapSideBName} subLine="Map" />
 			<main className={styles.main}>
 				<div className={styles.mainSection}>
 					<LinkCard />
-					<div className={styles.imgContainer}>
-						<Image src={"/img/parks-map.png"} width={"960"} height={"1355"} alt="Map of the parks around the Tofino area" />
+					<div>
+						<img className={styles.mapContainer} src={"/svg/maps/" + envMetadata.mapSideBDir} /* width={"960"} height={"1355"} */ alt="Map of the parks around the Tofino area" />
 					</div>
 				</div>
 			</main>
--- app/town-map/page.tsx
+++ app/town-map/page.tsx
@@ -2,16 +2,19 @@ import styles from "@/app/page.module.scss";
 import Image from "next/image";
 import Header from "@/components/Header";
 import LinkCard from "@/components/LinkCard";
+import { config } from "@/data/config";
+
+const envMetadata = config.envMetadata;
 
 export default function TownMap() {
 	return (
 		<div>
-			<Header mainLine="Tofino" subLine="Map" />
+			<Header mainLine={envMetadata.mapSideAName} subLine="Map" />
 			<main className={styles.main}>
 				<div className={styles.mainSection}>
 					<LinkCard />
-					<div className={styles.imgContainer}>
-						<Image src={"/img/tofino-map.png"} width={"960"} height={"1355"} alt="Map of the Town of Tofino" />
+					<div>
+						<img className={styles.mapContainer} src={"/svg/maps/" + envMetadata.mapSideADir} /* width={"1329"} height={"1618"} */ alt="Map of the Town of Tofino" />
 					</div>
 				</div>
 			</main>
